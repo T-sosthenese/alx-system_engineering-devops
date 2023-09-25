cd1cea0e433707987e4da30a7f7d4a283c0ffc68
sosthene <sosthenetimi94@gmail.com>
2023-09-25 13:16:50 +0300

    Dumping employee todos into .JSON

---
+++ 0x15-api/2-export_to_JSON.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""
+Accesses the todo list of employees based on their id
+and exports their information to a .JSON file.
+"""
+
+import json
+import requests
+import sys
+
+if __name__ == '__main__':
+    employeeId = sys.argv[1]
+    baseUrl = "https://jsonplaceholder.typicode.com/users"
+    url = baseUrl + "/" + employeeId
+
+    response = requests.get(url)
+    username = response.json().get('username')
+
+    todoUrl = url + "/todos"
+    response = requests.get(todoUrl)
+    tasks = response.json()
+
+    dictionary = {employeeId: []}
+    for task in tasks:
+        dictionary[employeeId].append({
+            "task": task.get('title'),
+            "completed": task.get('completed'),
+            "username": username
+        })
+    with open('{}.json'.format(employeeId), 'w') as f:
+        json.dump(dictionary, f)
